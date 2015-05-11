fe3fd6842bf6831e9213d8585087ab66885df415
Tiago Ilieve <tiago.myhro@gmail.com>
2015-05-10 23:54:15 -0300

    Remove example files

--- menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pcapfix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pcapfix" command="/usr/bin/pcapfix"
--- pcapfix.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pcapfix package
-#
-0 4	* * *	root	[ -x /usr/bin/pcapfix_maintenance ] && /usr/bin/pcapfix_maintenance
